105ce2d22c25eb3bcd7916dd66f085447181f9a5
Roi Martinez <roi.martinez@cefas.co.uk>
2020-04-17 04:08:53 +0100

    Update mssql_tacsat.sql

--- tacsat/mssql_tacsat.sql
+++ tacsat/mssql_tacsat.sql
@@ -5,22 +5,24 @@ Sat.Latitude as SI_LATI,
 Sat.Longitude as SI_LONG,
 CAST (Sat.SightingDate as DATE ) as SI_DATE,
 CAST (Sat.SightingDate as TIME ) as SI_TIME,
+Sat.SightingDate as SI_DATIM, 
 Sat.Speed as SI_SP,
 Sat.Course as SI_HE,
 null as SI_HARB,
 null as SI_STATE,
 CAST( isnull(iFV.VOYAGE_ID,0) AS BIGINT ) as SI_FT
 
-FROM (select * from dbo.F_VOYAGE  where VOYAGE_ID in (select ft_ref from rm12.ft_ref_uq_2018) )as iFV
-
+FROM dbo.F_VOYAGE as iFV
+--- select vessel details  ----
+inner join dbo.D_VESSEL iDV 
+on iFV.RSS_NO = iDV.RSS_NO and iDV.COUNTRY_CODE like 'GB%' 
+  and iFV.DEPARTURE_DATE_TIME between iDV.VALID_FROM_DATE and iDV.VALID_TO_DATE	
+inner join F_ACTIVITY iFA    
+	on iFA.VOYAGE_ID = iFV.VOYAGE_ID and  YEAR(ACTIVITY_DATE ) between 2019 and  2019 
 -- select VMS points from selected fishing voyages ---
-left outer  join  dbo.SatSighting Sat 
+inner outer  join  dbo.SatSighting Sat 
 on Sat.SightingDate between  iFV.DEPARTURE_DATE_TIME and iFV.RETURN_DATE_TIME 
 and Sat.RSSNo = iFV.RSS_NO
-
---- select vessel details  ----
-left outer join dbo.D_VESSEL iDV 
-on iFV.RSS_NO = iDV.RSS_NO and iFV.DEPARTURE_DATE_TIME between iDV.VALID_FROM_DATE and iDV.VALID_TO_DATE	
 
 -- filter fields with no voyage_id or no vessel  
 where iFV.VOYAGE_ID IS NOT NULL and Sat.VessReg is not null
