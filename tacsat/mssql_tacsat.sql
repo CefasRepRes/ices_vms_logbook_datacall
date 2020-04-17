b51300481d40495521b509bec2370b96f079b31a
Roi Martinez <roi.martinez@cefas.co.uk>
2020-04-17 04:14:13 +0100

    Update mssql_tacsat.sql

--- tacsat/mssql_tacsat.sql
+++ tacsat/mssql_tacsat.sql
@@ -17,13 +17,10 @@ FROM dbo.F_VOYAGE as iFV
 inner join dbo.D_VESSEL iDV 
 on iFV.RSS_NO = iDV.RSS_NO and iDV.COUNTRY_CODE like 'GB%' 
   and iFV.DEPARTURE_DATE_TIME between iDV.VALID_FROM_DATE and iDV.VALID_TO_DATE	
-inner join F_ACTIVITY iFA    
-	on iFA.VOYAGE_ID = iFV.VOYAGE_ID and  YEAR(ACTIVITY_DATE ) between 2019 and  2019 
 -- select VMS points from selected fishing voyages ---
 inner outer  join  dbo.SatSighting Sat 
 on Sat.SightingDate between  iFV.DEPARTURE_DATE_TIME and iFV.RETURN_DATE_TIME 
-and Sat.RSSNo = iFV.RSS_NO
+and Sat.RSSNo = iFV.RSS_NO and YEAR(Sat.SightingDate) = 2009
 
 -- filter fields with no voyage_id or no vessel  
-where iFV.VOYAGE_ID IS NOT NULL and Sat.VessReg is not null
-Order by  VE_REF,SI_FT,  SI_DATE, SI_TIME  
+
